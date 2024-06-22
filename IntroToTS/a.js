8acad24ae1a9dbda82bcdf7908a9a37c68c1676f
Sameer <sameer.marathe16536@sakec.ac.in>
2024-06-22 15:21:14 +0530

    Number -> number

---
+++ IntroToTS/a.js
@@ -0,0 +1,13 @@
+var x = 29;
+console.log(x);
+var y = "Sameer";
+console.log(y);
+var z = {
+    user: "Sameer",
+    age: 20
+};
+//Function:
+function hello() {
+    return "Hello World";
+}
+console.log(hello());
--- IntroToTS/a.ts
+++ IntroToTS/a.ts
@@ -1,2 +1,31 @@
-const x : Number = 29;
+const x : number = 29;
 console.log(x);
+
+const y : string = "Sameer"
+console.log(y)
+
+const z :object ={
+    user:"Sameer",
+    age : 20
+}
+
+//Function:
+function hello():string{
+    return "Hello World";
+}
+
+console.log(hello())
+
+//Greet Func
+function greet(name: string):string{
+    return ("Hello"+name)
+}
+console.log(greet("Sameer"))
+
+
+//add function:
+function add( a:number,b:number):number{
+    const result : number  = a + b;
+    return result
+}
+console.log(add(10,20))
